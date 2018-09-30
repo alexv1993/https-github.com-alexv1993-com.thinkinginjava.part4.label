dfe25b4083e9de580b165c5c41b14cec2119601f
alexv1993 <alexvendin@gmail.com>
2018-09-30 21:59:43 +0300

    labels and while

---
+++ src/main/java/com/thinkinginjava/part4/label/LabeledWhile.java
@@ -0,0 +1,35 @@
+package com.thinkinginjava.part4.label;
+
+/**
+ * Created by Alex on 30.09.2018.
+ */
+public class LabeledWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        outer:
+        while (true) {
+            System.out.println("Внешний цикл while");
+            while (true) {
+                i++;
+                System.out.println("i = " + i);
+                if (i == 1) {
+                    System.out.println("continue");
+                    continue;
+                }
+                if (i == 3) {
+                    System.out.println("continue outer");
+                    continue outer;
+                }
+                if (i == 5) {
+                    System.out.println("break");
+                    break;
+                }
+                if (i == 7) {
+                    System.out.println("break outer");
+                    break outer;
+                }
+
+            }
+        }
+    }
+}
